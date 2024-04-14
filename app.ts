fc400201373d8820915c4e751c55455ac8f6d5e1
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 10:05:17 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+//Solution # 7
+// Number Eight: write addition, substraction and division operation, that each result in the number 8. Be sure to enclose your 
+// operation in print statement to see the result.  
+
+
+let number = 5+3
+console.log(number);
+console.log(5+3)
+console.log(11-3)
+console.log(2*4)
+console.log(16/2)
